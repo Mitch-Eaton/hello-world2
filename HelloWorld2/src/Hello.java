864027d55dfac1b9b5fd130435045373e6fa2a9a
Mitch-Eaton <Mitchell.eaton@maine.edu>
2017-01-30 16:25:46 -0500

    Preamble added

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Here ye, here ye, an important announcement:");
         System.out.println("Hello, World!");
     }
 
