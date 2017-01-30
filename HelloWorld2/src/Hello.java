c8deb716e08577cabbfeba38fb56a5de108592ca
Mitch-Eaton <Mitchell.eaton@maine.edu>
2017-01-30 16:26:23 -0500

    Goodbye Added

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Here ye, here ye, an important announcement:");
         System.out.println("Hello, World!");
+        System.out.println("Thanks, goodbye!");
     }
 
 }
