fb584c09a9d3073a859f3df5ba77cebf63836130
Mitch-Eaton <Mitchell.eaton@maine.edu>
2017-02-04 20:19:42 -0500

    Unimportant preamble update

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Here ye, here ye, an important announcement:");
+        System.out.println("Here ye, here ye, an UNIMPORTANT announcement:");
         System.out.println("Hello, World!");
         System.out.println("Thanks, goodbye!");
     }
